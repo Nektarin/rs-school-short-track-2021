ef65fb532a04bf99da7bcc039bc78c2daecce8ce
Vlad Shutikov <vlad5565@mail.ru>
2021-04-07 14:17:28 +0300

    feat: complete task 03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,26 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const arrCopy = [];
+  let indexCopy = -1;
+
+  arr.map((value) => {
+    if (value !== -1) {
+      arrCopy.push(value);
+    }
+    return value;
+  });
+
+  arrCopy.sort((a, b) => (a - b));
+
+  return arr.map((item) => {
+    if (item !== -1) {
+      indexCopy++;
+      return arrCopy[indexCopy];
+    }
+    return item;
+  });
 }
 
 module.exports = sortByHeight;
