c60d9b4dfef4e003e99f29247f88935e40d987bb
Vlad Shutikov <vlad5565@mail.ru>
2021-04-09 14:36:57 +0300

    feat: complete task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,17 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let range = Math.floor(array.length / 2);
+  while (array[range] !== value) {
+    if (array[range] > value) {
+      range = Math.floor(range / 2);
+    } else {
+      range = Math.floor((array.length + range) / 2);
+    }
+  }
+
+  return range;
 }
 
 module.exports = findIndex;
