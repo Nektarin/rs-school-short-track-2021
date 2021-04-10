e23a56d465f85fbd2cd9f4e8bd4664d7f3c4b08c
Vlad <vshutikov10@gmail.com>
2021-04-10 13:52:15 +0300

    feat: complete task 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const set = new Set();
+  let str = '';
+  const obj = {};
+
+  domains.forEach((item) => {
+    const arr = item.split('.');
+    arr.reverse();
+    let conc = `.${arr[0]}`;
+    set.add(conc);
+    str += `${conc} `;
+    for (let index = 0; index < arr.length - 1; index++) {
+      conc += `.${arr[index + 1]}`;
+      str += `${conc} `;
+      set.add(conc);
+    }
+  });
+
+  const array = str.trim().split(' ');
+
+  set.forEach((value) => {
+    let n = 0;
+    array.forEach((item) => {
+      if (value === item) {
+        n++;
+      }
+    });
+    obj[`${value}`] = n;
+  });
+
+  return obj;
 }
 
 module.exports = getDNSStats;
