a29ef1e476d07a926f5140bbace4ad4a0212ead9
Vlad <vshutikov10@gmail.com>
2021-04-10 12:40:27 +0300

    feat: complete task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  const arr = [];
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index + 1] !== array[index]) {
+      arr.push(array[index]);
+    } else {
+      let i = index + 1;
+      let n = 2;
+      while (array[i] === array[i + 1]) {
+        n++;
+        i++;
+      }
+      arr.push(n + array[index]);
+      index = i;
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
