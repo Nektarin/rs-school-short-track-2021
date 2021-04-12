f067722ab572506305046e6a30b9c15dbca2dfeb
Vlad Shutikov <vlad5565@mail.ru>
2021-04-12 10:10:34 +0300

    feat: complete task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,53 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push(matrix[i].slice());
+  }
+
+  arr.map((item) => {
+    item.push(false);
+    item.unshift(false);
+    return item;
+  });
+
+  arr.unshift(new Array(arr[0].length).fill(false, 0, arr[0].length));
+  arr.push(new Array(arr[0].length).fill(false, 0, arr[0].length));
+
+  const arrCopy = [];
+  for (let i = 0; i < arr.length; i++) {
+    arrCopy.push(arr[i].slice());
+  }
+  for (let i = 1; i < arr.length - 1; i++) {
+    for (let j = 1; j < arr[i].length - 1; j++) {
+      let counter = 0;
+      for (let index = j - 1; index < 3; index++) {
+        if (arrCopy[i - 1][index] === true) {
+          counter++;
+        }
+        if (arrCopy[i + 1][index] === true) {
+          counter++;
+        }
+      }
+      if (arrCopy[i][j + 1] === true) {
+        counter++;
+      }
+      if (arrCopy[i][j - 1] === true) {
+        counter++;
+      }
+      arr[i][j] = counter;
+    }
+  }
+  arr.pop();
+  arr.shift();
+  arr.map((item) => {
+    item.pop();
+    item.shift();
+    return item;
+  });
+  return arr;
 }
 
 module.exports = minesweeper;
